3ffadefdf4c2bdb78ef4dee02938b16510e01030
Huy Hoang <huyhoang0496@gmail.com>
2018-12-19 00:35:58 +0100

    +

--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,39 +1,52 @@
-# 1 "c:\\Users\\Huy\\Documents\\GitHub\\Arduino\\potentiometer_delay\\potentiometer_delay.ino"
-# 1 "c:\\Users\\Huy\\Documents\\GitHub\\Arduino\\potentiometer_delay\\potentiometer_delay.ino"
+# 1 "c:\\Users\\Huy\\Documents\\GitHub\\Arduino\\src\\potentiometer_delay.ino"
+# 1 "c:\\Users\\Huy\\Documents\\GitHub\\Arduino\\src\\potentiometer_delay.ino"
 
 const int pin_pot = 0;
 const int pin_led = 3;
 
 int brightness = 0;
-int step_size = 5;
+int step = 0;
+int step_dir = 1;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(pin_led, 0x1);
+    Serial.begin(9600);
+    pinMode(pin_led, 0x1);
 }
 
 void loop() {
-//  int pot_val = analogRead(pin_pot);
-//  
-//  Serial.println(pot_val);
+    //  int pot_val = analogRead(pin_pot);
+    //
+    //  Serial.println(pot_val);
 
-//  if (brightness <= 0 || brightness >= 255) {
-//    step_size = -step_size;
-//  }
-
-  brightness += step_size;
-  Serial.println(brightness);
-
-  analogWrite(pin_led, brightness);
-
-  if (brightness <= 0) {
-    step_size = -step_size;
-  } else if(brightness >= 255) {
-    step_size = -step_size;
-    delay(1000);
-  }
+    //  if (brightness <= 0 || brightness >= 255) {
+    //    step_size = -step_size;
+    //  }
 
 
+    step += step_dir;
 
-  delay(10);
+    if (step <= 0) {
+        step_dir = -step_dir;
+        step = 0;
+        delay(1000);
+    }
+    else if (step >= 10) {
+        step_dir = -step_dir;
+    }
+
+    brightness = led_step(step);
+    analogWrite(pin_led, brightness);
+
+    Serial.println(brightness);
+    Serial.println(step);
+
+    delay(30);
+}
+
+int led_step(int step) {
+    int value = 255 - pow(step, 2);
+    if (value < 0) {
+        value = 0;
+    }
+    return value;
 }
--- build/sketch/potentiometer_delay.ino.cpp
+++ build/sketch/potentiometer_delay.ino.cpp
@@ -1,46 +1,60 @@
 #include <Arduino.h>
-#line 1 "c:\\Users\\Huy\\Documents\\GitHub\\Arduino\\potentiometer_delay\\potentiometer_delay.ino"
-#line 1 "c:\\Users\\Huy\\Documents\\GitHub\\Arduino\\potentiometer_delay\\potentiometer_delay.ino"
+#line 1 "c:\\Users\\Huy\\Documents\\GitHub\\Arduino\\src\\potentiometer_delay.ino"
+#line 1 "c:\\Users\\Huy\\Documents\\GitHub\\Arduino\\src\\potentiometer_delay.ino"
 
 const int pin_pot = 0;
 const int pin_led = 3;
 
 int brightness = 0;
-int step_size = 5; 
+int step = 0;
+int step_dir = 1;
 
-#line 8 "c:\\Users\\Huy\\Documents\\GitHub\\Arduino\\potentiometer_delay\\potentiometer_delay.ino"
+#line 9 "c:\\Users\\Huy\\Documents\\GitHub\\Arduino\\src\\potentiometer_delay.ino"
 void setup();
-#line 13 "c:\\Users\\Huy\\Documents\\GitHub\\Arduino\\potentiometer_delay\\potentiometer_delay.ino"
+#line 14 "c:\\Users\\Huy\\Documents\\GitHub\\Arduino\\src\\potentiometer_delay.ino"
 void loop();
-#line 8 "c:\\Users\\Huy\\Documents\\GitHub\\Arduino\\potentiometer_delay\\potentiometer_delay.ino"
+#line 44 "c:\\Users\\Huy\\Documents\\GitHub\\Arduino\\src\\potentiometer_delay.ino"
+int led_step(int step);
+#line 9 "c:\\Users\\Huy\\Documents\\GitHub\\Arduino\\src\\potentiometer_delay.ino"
 void setup() {
-  Serial.begin(9600);
-  pinMode(pin_led, OUTPUT);
+    Serial.begin(9600);
+    pinMode(pin_led, OUTPUT);
 }
 
 void loop() {
-//  int pot_val = analogRead(pin_pot);
-//  
-//  Serial.println(pot_val);
+    //  int pot_val = analogRead(pin_pot);
+    //
+    //  Serial.println(pot_val);
 
-//  if (brightness <= 0 || brightness >= 255) {
-//    step_size = -step_size;
-//  }
-
-  brightness += step_size;
-  Serial.println(brightness);
-
-  analogWrite(pin_led, brightness);
-  
-  if (brightness <= 0) {
-    step_size = -step_size;
-  } else if(brightness >= 255) {
-    step_size = -step_size;
-    delay(1000);
-  }
+    //  if (brightness <= 0 || brightness >= 255) {
+    //    step_size = -step_size;
+    //  }
 
 
-  
-  delay(10);
+    step += step_dir;
+
+    if (step <= 0) {
+        step_dir = -step_dir;
+        step = 0;
+        delay(1000);
+    } 
+    else if (step >= 10) {
+        step_dir = -step_dir;
+    }
+
+    brightness = led_step(step);
+    analogWrite(pin_led, brightness);
+
+    Serial.println(brightness);
+    Serial.println(step);
+
+    delay(30);
 }
 
+int led_step(int step) {
+    int value = 255 - pow(step, 2);
+    if (value < 0) {
+        value = 0;
+    }
+    return value;
+}
